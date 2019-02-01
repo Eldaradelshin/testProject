735847ffa87105df8c6b8eb36661f8a9fdfbe264
GoodBuy <e.adelshin@ya.ru>
2019-02-01 13:28:48 +0300

    initial

---
+++ testProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Eldar Adelshin on 01/02/2019.
+//  Copyright © 2019 Eldar Adelshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
