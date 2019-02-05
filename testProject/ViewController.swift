7143e2745e7562897aecf422664a84d3adb0aee7
GoodBuy <e.adelshin@ya.ru>
2019-02-05 14:06:36 +0300

    Fixed path for networking extensions

--- testProject/ViewController.swift
+++ testProject/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainLabel.text = "Hello new air!"
+        
     }
 
 
